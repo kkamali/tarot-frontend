5ff55823d6f808c2c9b62e5115b28d6e2f221506
Kathleen Kamali <kathleenkamali@gmail.com>
2020-05-09 14:24:50 -0700

    added api routing stuff

--- tarot-api/app/controllers/cards_controller.rb
+++ tarot-api/app/controllers/cards_controller.rb
@@ -1,2 +1,14 @@
 class CardsController < ApplicationController
+  def index
+    cards = Card.all
+    render json: cards
+  end
+
+  def show
+    card = Card.find(params[:id])
+  end
+
+  def card_params(params)
+    params.require(:card).permit(:id)
+  end
 end
