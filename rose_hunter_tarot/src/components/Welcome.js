010ced690e0fe92107667cad23f29239f9116a8e
Kathleen Kamali <kathleenkamali@gmail.com>
2020-05-20 14:11:20 -0700

    added spread and welcome comps

---
+++ rose_hunter_tarot/src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Welcome = props =>
+  <div>
+    <h1>Welcome!</h1>
+    <p>I'm so glad you're here! I'm the Tiny Rosehunter Tarot, and it  is <em>wonderful</em> to see you! I hooe I can be of service!</p>
+    <p>To get started, think of a question you have or a challenge you are facing. No problem is too small for the Tiny Rosehunter Tarot! I always do my best to offer helpful advice.</p>
+    <p>With your question in mind, next you must choose a spread. Each spread gives a different kind of reading, so be sure to pick one that makes sense for you today.</p>
+  </div>
+
+export default Welcome
