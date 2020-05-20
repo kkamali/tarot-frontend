f38a117ae77695f498941330018f696392b1250d
Kathleen Kamali <kathleenkamali@gmail.com>
2020-05-20 10:43:06 -0700

    adding drawing spreads

--- tarot-api/app/controllers/cards_controller.rb
+++ tarot-api/app/controllers/cards_controller.rb
@@ -9,7 +9,46 @@ class CardsController < ApplicationController
     render json: card
   end
 
+  def spread
+    spread = params[:spread]
+    cards = []
+    case spread
+    when 'dragonfly'
+      cards = make_spread(9)
+    when 'advice'
+      cards = make_spread(3)
+    when 'love'
+      cards = make_spread(6)
+    when 'dreaming'
+      cards = make_spread(5)
+    when 'pathways'
+      cards = make_spread(8)
+    end
+    if cards.length > 0
+      render json: cards
+    else
+      render json: {error: "No spread selected"}
+    end
+  end
+
+  private
+
   def card_params(params)
-    params.require(:card).permit(:id)
+    params.require(:card).permit(:id, :spread)
+  end
+
+  def draw_card
+    Card.find(rand(1..17))
+  end
+
+  def make_spread(cards_needed)
+    drawn = []
+    while drawn.length < cards_needed
+      card = draw_card()
+      if !drawn.include? card
+        drawn << card
+      end
+    end
+    drawn
   end
 end
--- tarot-api/config/routes.rb
+++ tarot-api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :cards
+  resources :cards, only: [:index, :show]
+  post '/spreads/:spread', to: 'cards#spread'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
