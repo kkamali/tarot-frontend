38ef5e29ecc8f6ba151d4d5cb79d826f99141de8
Kathleen Kamali <kathleenkamali@gmail.com>
2020-05-20 15:01:01 -0700

    changed to cards

--- rose_hunter_tarot/src/components/Spreads.js
+++ rose_hunter_tarot/src/components/Spreads.js
@@ -1,24 +1,50 @@
 import React from 'react'
 
 export const Spreads = props =>
-  <div>
+  <Container>
     <h1>Here are the spreads I know!</h1>
-    <div>
-      <p>
-        <strong>Classic Dragonfly: </strong>This 9-card spread is the traditional choice for Rosehunter defcks. It is a multi-faceted, general-use spread that digs into the details of a situation.</p>
-      <p>
-        <strong>Aunty's Advice: </strong>This 3-card spread is perfect for when you're in a rush and just need a quick check-in! Much like advice from a stern but loving aunt, this spread is concise, insightful, and versatile!
-      </p>
-      <p>
-        <strong>True Love: </strong>This 6-card spread explores the strengths, weaknesses, and challenges of your relationships. It was designed for use in romantic contexts, but it can also be used for non-romantic relationships!
-      </p>
-      <p>
-        <strong>Dreaming: </strong>As the title implies, this 5-card spread helps demystify your dreams and find out what your subconsious is trying to tell you!
-      </p>
-      <p>
-        <strong>Pathways: </strong>This 8-card spread is used to find clarity when you are faced with a dichotomous decision. It explores the pros and cons of each option and helps you see the full picture.
-      </p>
-    </div>
-  </div>
+    <Container>
+      <Card>
+        <Card.Body>
+          <Card.Title>Classic Dragonfly</Card.Title>
+          <Card.Text>
+            This 9-card spread is the traditional choice for Rosehunter defcks. It is a multi-faceted, general-use spread that digs into the details of a situation.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Aunty's Advice</Card.Title>
+          <Card.Text>
+            This 3-card spread is perfect for when you're in a rush and just need a quick check-in! Much like advice from a stern but loving aunt, this spread is concise, insightful, and versatile!
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>True Love</Card.Title>
+          <Card.Text>
+            This 6-card spread explores the strengths, weaknesses, and challenges of your relationships. It was designed for use in romantic contexts, but it can also be used for non-romantic relationships!
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Dreaming</Card.Title>
+          <Card.Text>
+            As the title implies, this 5-card spread helps demystify your dreams and find out what your subconsious is trying to tell you!
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Pathways</Card.Title>
+          <Card.Text>
+            As the title implies, this 5-card spread helps demystify your dreams and find out what your subconsious is trying to tell you!
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  </Container >
 
 export default Spreads
