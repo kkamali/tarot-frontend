01cbe407d904a51b07a2108f11f3e66b6d7b2d5f
Kathleen Kamali <kathleenkamali@gmail.com>
2020-05-21 11:32:15 -0700

    grid city woo

--- rose_hunter_tarot/src/components/Deck.js
+++ rose_hunter_tarot/src/components/Deck.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container } from '@material-ui/core'
+import { makeStyles, Grid, Container } from '@material-ui/core'
 
 import TarotCard from './TarotCard'
 
@@ -20,9 +20,20 @@ class Deck extends Component {
 
   render() {
     return (
-      <Container>
-        {this.state.tarotCards.map(card => <TarotCard name={card.name} img_link={card.img} upright={card.upright} reversed={card.reversed} />)}
-      </Container>
+      <Grid container style={{ flexGrow: 1 }} spacing={2}>
+        <Grid item xs={this.state.tarotCards.length}>
+          <Grid container justify="center" spacing={2}>
+            {this.state.tarotCards.map((card) => (
+              <Grid key={card} item>
+                <TarotCard name={card.name} img_link={card.img} upright={card.upright} reversed={card.reversed} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+      // <Container>
+      //   {this.state.tarotCards.map(card => <TarotCard name={card.name} img_link={card.img} upright={card.upright} reversed={card.reversed} />)}
+      // </Container>
     )
   }
 }
