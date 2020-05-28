6fe1f187ed79de0a9dfe81c9f22c7f01c7591707
Kathleen Kamali <kathleenkamali@gmail.com>
2020-05-27 19:44:25 -0700

    initial commit for deployment

---
+++ src/components/Pathways.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import Drawn from './Drawn'
+import { Grid } from '@material-ui/core'
+
+export default function Pathways() {
+  const [drawn, setDrawn] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  async function fetchCards() {
+    const response = await fetch('/spreads/pathways')
+    const json = await response.json()
+
+    setDrawn(json)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchCards()
+  }, [])
+
+
+  return (
+    <>
+      {loading ? (
+        "Reading the cards..."
+      ) : (
+          <Grid container justify="center">
+
+          </Grid>
+        )}
+    </>
+  )
+}
