4e5868148b91180ecd19cf6d5ce9e5877ff1a5f7
Kathleen Kamali <kathleenkamali@gmail.com>
2020-05-28 13:25:47 -0700

    added pathways spread

--- src/components/Pathways.js
+++ src/components/Pathways.js
@@ -25,7 +25,34 @@ export default function Pathways() {
         "Reading the cards..."
       ) : (
           <Grid container justify="center">
-
+            <Grid container justify="center">
+              <Grid item>
+                <Drawn card={drawn[0]} spread_pos="Currently" spread_meaning="The path you're on now" />
+              </Grid>
+              <Grid item>
+                <Drawn card={drawn[1]} spread_pos="Benefits" spread_meaning="Upsides to staying the course" />
+              </Grid>
+              <Grid item>
+                <Drawn card={drawn[2]} spread_pos="Drawbacks" spread_meaning="Disadvantages to staying the course" />
+              </Grid>
+              <Grid item>
+                <Drawn card={drawn[3]} spread_pos="Outcome" spread_meaning="Eventual result of staying the course" />
+              </Grid>
+            </Grid>
+            <Grid container justify="center">
+              <Grid item>
+                <Drawn card={drawn[4]} spread_meaning="The path you could choose" spread_pos="Change" />
+              </Grid>
+              <Grid item>
+                <Drawn card={drawn[5]} spread_pos="Benefits" spread_meaning="Upsides to changing your course" />
+              </Grid>
+              <Grid item>
+                <Drawn card={drawn[6]} spread_pos="Disadvantages to changing your course" />
+              </Grid>
+              <Grid item>
+                <Drawn card={drawn[7]} spread_meaning="Eventual result of changing your course" />
+              </Grid>
+            </Grid>
           </Grid>
         )}
     </>
